2ea3c66ff1b2365eede6dc3487cc8565d1ba3a26
litsungyi <litsungyi@gmail.com>
2018-08-19 02:20:40 +0800

    Update resume

--- js/education_and_experience.js
+++ js/education_and_experience.js
@@ -2,17 +2,24 @@ var educationBranchText = "education";
 var experienceBranchText = "experience";
 var igsBranchText = "igs";
 var maxonBranchText = "maxon";
+var sigonoBranchText = "sigono";
 
 var bsText = "逢甲大學電機系 (2000-2004)";
 var msText = "逢甲大學資工所 (2004-2006)";
-var igsText = "鈊象電子 (軟體工程師: 2008-2011; 資深軟體工程師: 2011-2014; 主任: 2014)";
-var maxonText = "麥成文創 (軟體架構師: 2014-2015)";
+var igsText1 = "鈊象電子 (軟體工程師: 200802-201103)";
+var igsText2 = "鈊象電子 (資深軟體工程師: 201103-201402)";
+var igsText3 = "鈊象電子 (軟體主任: 201403-201405)";
+var maxonText = "麥成文創 (軟體架構師: 201405-201511)";
+var sigonoText = "信革數位 (資深工程師: 201601-201808)";
 
 var sdkProjText = "GameTower SDK";
-var mjProjText = "明星三缺一 Win8";
-var vegaProjText = "織女星寵 (APP)";
-var galileeProjText = "加利利旅遊 (APP)";
-var ichibanProjText = "百萬智多星 (APP)";
+var mjProjText = "明星三缺一 Win8 (WP)";
+var vegaProjText = "織女星寵 (Android)";
+var galileeProjText = "加利利旅遊 (Android/iOS)";
+var ichibanProjText = "百萬智多星 (Android/iOS)";
+var beyondProjText = "地球計畫 (Android/iOS/Steam/Switch/MacOS)";
+var heavenProjText = "靈魂之橋 (Android/iOS/Steam/Switch/MacOS)";
+
 
 var gitgraph = new GitGraph({
   template: "metro",
@@ -23,12 +30,16 @@ var gitgraph = new GitGraph({
 var educationBranch = gitgraph.branch( educationBranchText );
 educationBranch.commit( bsText ).commit( msText );
 var experienceBranch = educationBranch.branch( experienceBranchText );
-experienceBranch.commit( igsText );
+experienceBranch.commit( igsText1 );
 var igsJobBranch = experienceBranch.branch( igsBranchText );
 igsJobBranch.commit( sdkProjText ).commit( mjProjText );
+experienceBranch.commit( igsText2 );
+experienceBranch.commit( igsText3 );
 igsJobBranch.merge( experienceBranch );
 experienceBranch.commit( maxonText );
 var maxonJobBranch = experienceBranch.branch( maxonBranchText );
 maxonJobBranch.commit( vegaProjText ).commit( galileeProjText ).commit( ichibanProjText );
 maxonJobBranch.merge( experienceBranch );
-educationBranch.commit( "改變成真" );
+var sigonoJobBranch = experienceBranch.branch( sigonoBranchText );
+sigonoJobBranch.commit( beyondProjText ).commit( heavenProjText );
+sigonoJobBranch.merge( experienceBranch );
